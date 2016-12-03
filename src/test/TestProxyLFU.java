e118bc3c9baffcb483e4c7fd7032025812b44b6c
Mike Soto <sotom1@nku.edu>
2016-12-03 14:36:52 -0500

    Update TestProxyLFU.java
    Added file output for run time.

--- src/test/TestProxyLFU.java
+++ src/test/TestProxyLFU.java
@@ -1,12 +1,10 @@
 package test;
 
 import static org.junit.Assert.*;
-
 import java.io.File;
-
-import org.junit.Before;
+import java.io.IOException;
+import java.io.PrintWriter;
 import org.junit.Test;
-
 import lruproxy.CacheList;
 import lruproxy.Proxy;
 
@@ -19,19 +17,23 @@ public class TestProxyLFU {
 	String inDirectory;
 	Proxy proxy;
 	long startTime, endTime;
-	
-	@Before
-	public void setUp() {
-		inDirectory = "data" + File.separator;
-		proxy = new Proxy(inDirectory, 5, 1, CacheList.LFU);
-	}
 
 	@Test
 	public void test() {
+		inDirectory = "data" + File.separator;
+		String[] args = { inDirectory, "3", "0", ""+CacheList.LFU};
+		
 		System.out.println("Executing TestProxyLFU");
 		startTime = System.currentTimeMillis();
-		proxy.run();
+		Proxy.main(args);
 		endTime = System.currentTimeMillis();
+		
+		try{
+		    PrintWriter writer = new PrintWriter("data"+File.separator+"nonfunc_output.txt", "UTF-8");
+		    writer.println("LFU Time: "+(endTime-startTime));
+		    writer.close();
+		} catch (IOException e) {}
+		
 		System.out.println("\nTime to execute: " + String.valueOf(endTime - startTime));
 	}
 
